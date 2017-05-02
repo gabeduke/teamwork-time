b280b006c294767c739090ae6d494fec88e6c1ec
Dukeman <gduke@bullhorn.com>
2017-05-02 09:57:56 -0400

    establishing structure

--- ttime.py
+++ ttime.py
@@ -1,13 +1,12 @@
 import configparser
 
 import easyargs
-import pudb
 from cursesmenu import *
 from cursesmenu.items import FunctionItem
 
 from ttime import teamwork
-from ttime.conf_helper import *
-from pick import pick
+from ttime.conf_helper import initialize_config
+from ttime.user_helper import get_projects, select_projects
 
 configfile_name = "config.ini"
 
@@ -18,8 +17,9 @@ initialize_config(config, configfile_name)
 project_ids = {}
 base_uri = config['user']['company'] + ".teamwork.com"
 api_key = config['user']['api_key']
-instance = teamwork.Teamwork(base_uri, api_key)
 
+
+instance = teamwork.Teamwork(base_uri, api_key)
 
 @easyargs
 def main(rebuild_projects="false"):
@@ -27,41 +27,11 @@ def main(rebuild_projects="false"):
     teamwork time wootwoot
     :param rebuild_projects: rebuild the project list cache
     """
-    if rebuild_projects == "true":
+    if rebuild_projects != "false":
         print('###REBUILDING PROJECTS###')
-        get_projects()
+        #get_projects()
 
     menu()
-
-
-def get_projects():
-    projects = instance.get_projects()
-    for project in projects:
-        project_ids[project['name']] = project['id']
-
-    print("###ADDING PROJECT LIST TO CONFIG###")
-    add_config(config, "projects", project_ids)
-
-    # pudb.set_trace()
-    for project_name, project_id in project_ids.items():
-        tasks = instance.get_project_tasks(int(project_id))
-        tasks_ids = {}
-
-        for task in tasks:
-            tasks_ids[task['todo-list-name']] = task['todo-list-id']
-
-        print("###ADDING {} TASKS TO CONFIG###".format(project_name).upper())
-        add_config(config, project_name, tasks_ids)
-
-
-def select_projects():
-    title = "###PROJECT LIST###"
-    options = []
-    config_dict = dict(config.items('projects'))
-    for key, value in config_dict:
-        options.append(key)
-
-    option, index = pick(options, title)
 
 
 def menu():
@@ -69,8 +39,8 @@ def menu():
     this_menu = CursesMenu("Teamwork Time", "Menu")
 
     # create the menu items
-    rebuild_projects = FunctionItem("Rebuild Project Cache", get_projects())
-    select_project = FunctionItem("Select Project", select_projects())
+    rebuild_projects = FunctionItem("Rebuild Project Cache", get_projects(instance, project_ids, config))
+    select_project = FunctionItem("Select Project", select_projects(config))
 
     # build the menu
     this_menu.append_item(rebuild_projects)
@@ -81,4 +51,4 @@ def menu():
 
 
 if __name__ == "__main__":
-    main()
+    menu()
---
+++ ttime/user_helper.py
@@ -0,0 +1,34 @@
+from pick import pick
+
+from ttime.conf_helper import add_config
+
+
+def get_projects(instance, project_ids, config):
+    projects = instance.get_projects()
+    for project in projects:
+        project_ids[project['name']] = project['id']
+
+    print("###ADDING PROJECT LIST TO CONFIG###")
+    add_config(config, "projects", project_ids)
+
+    for project_name, project_id in project_ids.items():
+        tasks = instance.get_project_tasks(int(project_id))
+        tasks_ids = {}
+
+        for task in tasks:
+            tasks_ids[task['todo-list-name']] = task['todo-list-id']
+
+        print("###ADDING {} TASKS TO CONFIG###".format(project_name).upper())
+        add_config(config, project_name, tasks_ids)
+
+
+def select_projects(config):
+    title = "###PROJECT LIST###"
+    options = []
+    config_dict = dict(config.items('projects'))
+    for key, value in config_dict.items():
+        options.append(key)
+
+    option, index = pick(options, title)
+    print(option)
+    print(index)
